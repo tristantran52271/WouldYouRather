f470fde2436b41ed78fb1619ef56be22f2385d51
Tristan Tran <tristantran@Tristans-MacBook-Pro.local>
2019-05-08 10:18:04 +1000

    Main/Login/Create Account Page and navigation for each

---
+++ Would You Rather/main.js
@@ -0,0 +1,8 @@
+goToPage(mainPage);
+
+function goToPage(pageNumber) {
+	//hides all pages with a class of pages
+	document.querySelectorAll('.pages').forEach((e) => e.hidden = true);
+	//shows page needed
+	pageNumber.hidden = false;
+}
