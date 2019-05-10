e17371806eb9fb4035b0e9198b8170ef79c38584
Tristan Tran <tristantran@Tristans-MacBook-Pro.local>
2019-05-10 20:16:27 +1000

    Firebase!

--- Would You Rather/main.js
+++ Would You Rather/main.js
@@ -1,4 +1,27 @@
-goToPage(answerQuestionsPage);
+goToPage(menuPage);
+
+let userArray = [];
+var database = firebase.database();
+let userArrayRef = database.ref('/userArray');
+
+userArrayRef.once('value').then(reload);
+
+function reload(data) {
+	if (userArray == null) userArray = []; //If there is no data in the online databse, it creates an array
+	userArray = data.val();//Adding the incoming data into a object
+	//console.log(userArray);
+	userArrayRef.update(userArray);
+	console.log(data.val());
+}
+//userArrayRef.update(userArray);
+
+function createAccount() {
+	let createAccountUsernameInput = document.getElementById("createAccountUsernameInput").value;
+	let createAcconutPasswordInput = document.getElementById("createAccountPasswordInput").value;
+
+	userArray.push([createAccountUsernameInput, createAcconutPasswordInput]);
+	userArrayRef.update(userArray);
+}
 
 function goToPage(pageNumber) {
 	//hides all pages with a class of pages
