2728c48c2f7308a83a970acf6add477cf3521214
Tristan Tran <tristantran@Tristans-MacBook-Pro.local>
2019-05-14 10:23:07 +1000

    Error Text

--- Would You Rather/main.js
+++ Would You Rather/main.js
@@ -21,6 +21,7 @@ function createAccount() {
 	// Grabs the values from the HTML inputs
 	let createAccountUsernameInput = document.getElementById("createAccountUsernameInput").value;
 	let createAcconutPasswordInput = document.getElementById("createAccountPasswordInput").value;
+	let createAccountErrorText = document.getElementById("createAccountErrorText");
 
 	// Checks if the user is connected to the internet, if not, exit the function
 	let isOnline = window.navigator.onLine;
@@ -28,12 +29,14 @@ function createAccount() {
 		console.log("online");
 	} else {
 		console.log("No internet connection");
+		createAccountErrorText.innerHTML = "No internet connection";
 		return;
 	}
 
 	// Checks whether or not there are any inputted values, if so exit the function
 	if (createAccountUsernameInput === "" || createAcconutPasswordInput === "") {
 		console.log("Please fill all fields!");
+		createAccountErrorText.innerHTML = "Please fill all fields";
 		return;
 	}
 
@@ -41,6 +44,7 @@ function createAccount() {
 	let hasSpaces = createAccountUsernameInput.toString().includes(" ");
 	if (hasSpaces) {
 		console.log("Please remove any spaces in the username");
+		createAccountErrorText.innerHTML = "Please remove any spaces in the username";
 		return;
 	}
 
@@ -48,6 +52,7 @@ function createAccount() {
 	for (i = 0; i < userArray.length; i++) {
 		if (userArray[i][0] === createAccountUsernameInput) {
 			console.log("Username already exist!");
+			createAccountErrorText.innerHTML = "Username already exist";
 			return;
 		}
 	}
@@ -60,6 +65,17 @@ function login() {
 	// Grabs the values from the HTML inputs
 	let loginUsernameInput = document.getElementById("loginUsernameInput").value;
 	let loginPasswordInput = document.getElementById("loginPasswordInput").value;
+	let loginErrorText = document.getElementById("loginErrorText");
+
+	// Checks if the user is connected to the internet, if not, exit the function
+	let isOnline = window.navigator.onLine;
+	if (isOnline) {
+		console.log("online");
+	} else {
+		console.log("No internet connection");
+		loginErrorText.innerHTML = "No internet connection";
+		return;
+	}
 
 	// Checks the user database
 	for (i = 0; i < userArray.length; i++) {
@@ -71,6 +87,7 @@ function login() {
 		} else {
 			// Otherwise, the user does not have an account
 			console.log("Account does not exist!")
+			loginErrorText.innerHTML = "Account does not exist";
 			return;
 		}
 	}
@@ -82,7 +99,15 @@ function goToPage(pageNumber) {
 	document.querySelectorAll('.pages').forEach((e) => e.hidden = true);
 
 	// Clear the inputs when the user enters the page
+	if (pageNumber === loginPage) {
+		document.getElementById("loginErrorText").innerHTML = "";
+		document.getElementById("loginUsernameInput").value = "";
+		document.getElementById("loginPasswordInput").value = "";
+	}
+
+	// Clear the inputs when the user enters the page
 	if (pageNumber === createAccountPage) {
+		document.getElementById("createAccountErrorText").innerHTML = "";
 		document.getElementById("createAccountUsernameInput").value = "";
 		document.getElementById("createAccountPasswordInput").value = "";
 	}
